2eb1aba8baae5e679862e547a093266b54445b69
ibuildthehealth <ibuildthehealth@easylifewell.com>
2016-10-16 01:08:33 +0000

    add gopkg.in/mgo.v2 and mongodb operation
    Signed-off-by: ibuildthehealth <ibuildthehealth@easylifewell.com>

--- data.go
+++ data.go
@@ -1,8 +1,120 @@
 package main
 
-type Data struct {
-	ID    int
-	Value string
-}
+import (
+	"time"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	URL = "127.0.0.1:27017"
+)
 
 type Datas []Data
+
+type Data struct {
+	ID       bson.ObjectId `json:"_id" bson:"_id"`
+	Version  string        `json:"Version" bson:"version"`
+	DeviceID int           `json:"ID" bson:"device_id"`
+	Temp     float64       `json:"TEMP" bson:"temperature"`
+	Humi     float64       `json:"HUMI" bson:"humidity"`
+	Form     float64       `json:"FORM" bson:"formaldehyde"`
+	Pm25     float64       `json:"PM2.5" bson:"pm25"`
+	Time     time.Time     `bson:"time"`
+	Value    string
+}
+
+var (
+	mgoSession *mgo.Session
+	dataBase   = "YuanView"
+	collection = "yuanview"
+)
+
+/**
+ * 公共方法，获取session，如果存在则拷贝一份
+ */
+func getSession() *mgo.Session {
+	if mgoSession == nil {
+		var err error
+		mgoSession, err = mgo.Dial(URL)
+		if err != nil {
+			panic(err) //直接终止程序运行
+		}
+	}
+	//最大连接池默认为4096
+	return mgoSession.Clone()
+}
+
+//公共方法，获取collection对象
+func witchCollection(collection string, s func(*mgo.Collection) error) error {
+	session := getSession()
+	defer session.Close()
+	c := session.DB(dataBase).C(collection)
+	return s(c)
+}
+
+// AddData 添加Data对象
+func AddData(p Data) string {
+	p.ID = bson.NewObjectId()
+	query := func(c *mgo.Collection) error {
+		return c.Insert(p)
+	}
+	err := witchCollection(collection, query)
+	if err != nil {
+		return "false"
+	}
+	return p.ID.Hex()
+}
+
+// GetDataByID 获取一条记录通过objectid
+func GetDataByID(id string) *Data {
+	objid := bson.ObjectIdHex(id)
+	data := new(Data)
+	query := func(c *mgo.Collection) error {
+		return c.FindId(objid).One(&data)
+	}
+	witchCollection(collection, query)
+	return data
+}
+
+// GetDatas 获取所有的data数据
+func GetDatas() []Data {
+	var datas []Data
+	query := func(c *mgo.Collection) error {
+		return c.Find(nil).All(&datas)
+	}
+	err := witchCollection(collection, query)
+	if err != nil {
+		return datas
+	}
+	return datas
+}
+
+// UpdateData 更新data数据
+func UpdateData(query bson.M, change bson.M) string {
+	exop := func(c *mgo.Collection) error {
+		return c.Update(query, change)
+	}
+	err := witchCollection(collection, exop)
+	if err != nil {
+		return "true"
+	}
+	return "false"
+}
+
+// SearchData 执行查询，此方法可拆分做为公共方法
+// [SearchData description]
+// @param {[type]} collectionName string [description]
+// @param {[type]} query          bson.M [description]
+// @param {[type]} sort           bson.M [description]
+// @param {[type]} fields         bson.M [description]
+// @param {[type]} skip           int    [description]
+// @param {[type]} limit          int)   (results      []interface{}, err error [description]
+func SearchData(collectionName string, query bson.M, sort string, fields bson.M, skip int, limit int) (results []interface{}, err error) {
+	exop := func(c *mgo.Collection) error {
+		return c.Find(query).Sort(sort).Select(fields).Skip(skip).Limit(limit).All(&results)
+	}
+	err = witchCollection(collectionName, exop)
+	return
+}
--- repo.go
+++ repo.go
@@ -13,7 +13,7 @@ func init() {
 
 func RepoFindData(id int) Data {
 	for _, d := range datas {
-		if d.ID == id {
+		if d.DeviceID == id {
 			return d
 		}
 	}
@@ -23,14 +23,14 @@ func RepoFindData(id int) Data {
 
 func RepoCreateData(d Data) Data {
 	currentID++
-	d.ID = currentID
+	d.DeviceID = currentID
 	datas = append(datas, d)
 	return d
 }
 
 func RepoDestroyData(id int) error {
 	for i, d := range datas {
-		if d.ID == id {
+		if d.DeviceID == id {
 			datas = append(datas[:i], datas[i+1:]...)
 			return nil
 		}
